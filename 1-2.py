8ec37b748c0d0762d35fe87d579a6eb108b3bca7
jhjenbaik <jaehyobaik@gmail.com>
2026-05-30 20:47:09 +0800

    Remove matplotlib import from 1-2.py

--- 1-2.py
+++ 1-2.py
@@ -9,7 +9,6 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 import streamlit as st
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from scipy.stats import norm
 
